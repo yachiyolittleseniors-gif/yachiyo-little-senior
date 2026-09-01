14b89ec2b9d7b7ba19f3750fc3c6094476b4c840
yachiyolittleseniors-gif <yachiyo.littleseniors@gmail.com>
2026-09-01 11:47:00 +0900

    Update site-data.mjs

--- netlify/functions/site-data.mjs
+++ netlify/functions/site-data.mjs
@@ -1,6 +1,6 @@
 import { getStore } from "@netlify/blobs";
 
-const allowed = new Set(["schedule", "results", "gallery", "players", "hero", "photos", "news"]);
+const allowed = new Set(["schedule", "results", "gallery", "players", "hero", "photos", "news", "rules"]);
 
 function json(data, status = 200) {
   return new Response(JSON.stringify(data), {
